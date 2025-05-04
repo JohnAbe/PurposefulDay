3dc14101258f5b604bb61d74bc1d0f04e6ab7396
John Abraham <johnabraham91@gmail.com>
2025-05-04 14:40:40 -0700

    Initial Commit

---
+++ PurposefulDay/PurposefulDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurposefulDayApp.swift
+//  PurposefulDay
+//
+//  Created by John  Abraham on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PurposefulDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PurposefulDayTests/PurposefulDayTests.swift
@@ -0,0 +1,17 @@
+//
+//  PurposefulDayTests.swift
+//  PurposefulDayTests
+//
+//  Created by John  Abraham on 5/4/25.
+//
+
+import Testing
+@testable import PurposefulDay
+
+struct PurposefulDayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
